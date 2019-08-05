b27cf8a72ef30e9ec26d013777616182c004619e
2316035851 <53028655+2316035851@users.noreply.github.com>
2019-08-05 20:39:13 +0800

    Add files via upload

---
+++ Narcissus.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Narcissus{
+	public static void main(String[] args){
+		for(int i=100;i<1000;i++){
+			int result = (int)Math.pow(i%10,3)+(int)Math.pow(i/10%10,3)+(int)Math.pow(i/100,3);
+			if(i == result){
+				System.out.print(i);
+			}
+		}
+	}
+}
